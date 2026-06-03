f1e3676cbd9103727da834d475bf663d5bf14320
Corpame <root@corpa.me>
2026-06-03 13:45:34 +0800

    Remove legacy workspace network artifacts

--- workspace/flush_rule_table.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for t in 101 102 103 104; do
-    ip route flush table $t
-done
-
-for t in 101 102 103 104; do
-    ip rule flush table $t
-done
--- workspace/install_container.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /home/wangxuanqi/shanghai/lib_xpu_container_1.0.2-1/ubuntu22.04/amd64
-dpkg -i libxpu-container-tools_1.0.2-1_amd64.deb 
-dpkg -i libxpu-container1_1.0.2-1_amd64.deb
-cd /home/wangxuanqi/shanghai/xpu_container_1.0.5-1/ubuntu22.04/amd64
-dpkg -i xpu-container-toolkit_1.0.5-1_amd64.deb 
